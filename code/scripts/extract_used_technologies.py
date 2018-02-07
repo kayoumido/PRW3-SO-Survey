427ab6a3357b7254e7516c68774a54a50df0adad
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-02-07 11:44:35 +0100

    Updated script so it extracts a count of each tech

--- code/scripts/extract_used_technologies.py
+++ code/scripts/extract_used_technologies.py
@@ -5,6 +5,10 @@ def extract_used_technologies():
     """Used technologies extractor
     Extract the used technologies from the raw dataset
     and stores them in a new csv file.
+
+    It's import to execute this script at the root of the project.
+    Otherwise the paths to the different csv files won't work.
+    If you're stubborn you can change the paths.
     """
 
     wanted_cols = [
@@ -13,23 +17,32 @@ def extract_used_technologies():
         "HaveWorkedDatabase",
         "HaveWorkedPlatform",
     ]
-
+    # open raw data
     df = pd.read_csv('data/raw/survey_results_public.csv')
     # opening/creating output file
     output = open('data/processed/extracted_used_technologies.csv', 'w+')
-    output.write('Respondent,Used Technologies\n')
+    output.write('Technologie,Count\n')
+
+    technologies = {}
 
     for index, row in df.iterrows():
-        respondent = row['Respondent']
 
         for col in wanted_cols:
+            # split string of techs into array
             techs = str(row[col]).split(';')
 
+            # count different techs
             for tech in techs:
+                tech = tech.lstrip()
+                # skip nan
                 if tech != "nan":
-                    output.write(
-                        str(respondent) + ',' + str(tech).lstrip() + '\n'
-                    )
+                    technologies[tech] = technologies.get(tech, 0) + 1
+
+    # write techs in output file
+    for technologie, count in technologies.items():
+        output.write(
+            technologie + ',' + str(count) + '\n'
+        )
 
 
 if __name__ == "__main__":
