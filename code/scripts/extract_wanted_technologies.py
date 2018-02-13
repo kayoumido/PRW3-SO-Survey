7776e9b887c993fc5ef32afd60706355d29193b3
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-02-13 14:01:44 +0100

    Changed extract_wanted_technologies
    Original script didn't get the needed data. Now it returns the technologies wanted to be used by people using a certain type of technologie (Yeah I know its a bit WTF)

--- code/scripts/extract_wanted_technologies.py
+++ code/scripts/extract_wanted_technologies.py
@@ -3,33 +3,49 @@ import pandas as pd
 
 def extract_wanted_technologies():
     """Wanted to use technologies exractor
-    Extract the wanted to use technologies from the raw dataset
-    and stores them in a new csv file.
+    Extract the wanted to use technologies for certain used technologies
+    from the raw dataset and stores them in a new csv file.
     """
 
-    wanted_cols = [
-        "WantWorkLanguage",
-        "WantWorkFramework",
-        "WantWorkDatabase",
-        "WantWorkPlatform",
-    ]
-
+    wanted_cols = {
+        "HaveWorkedLanguage": "WantWorkLanguage",
+        "HaveWorkedFramework": "WantWorkFramework",
+        "HaveWorkedDatabase": "WantWorkDatabase",
+        "HaveWorkedPlatform": "WantWorkPlatform",
+    }
     df = pd.read_csv('data/raw/survey_results_public.csv')
     # opening/creating output file
     output = open('data/processed/extracted_wanted_technologies.csv', 'w+')
-    output.write('Respondent,Wanted Technologies\n')
+    output.write('Used Technologie,Wanted Technologie,Count\n')
+
+    technologies = {}
 
     for index, row in df.iterrows():
-        respondent = row['Respondent']
 
-        for col in wanted_cols:
-            techs = str(row[col]).split(';')
+        for used, wanted in wanted_cols.items():
+            # split used techs (utechs) and wanted techs (wtechs)
+            utechs = str(row[used]).split(';')
+            wtechs = str(row[wanted]).split(';')
 
-            for tech in techs:
-                if tech != "nan":
-                    output.write(
-                        str(respondent) + ',' + str(tech).lstrip() + '\n'
-                    )
+            for utech in utechs:
+                if utech == 'nan':
+                    continue
+
+                for wtech in wtechs:
+                    technologies[utech] = technologies.get(utech, {})
+                    technologies[utech][wtech] = technologies[utech].get(
+                        wtech, 0) + 1
+
+    for utech, wtechs in technologies.items():
+        for wtech, count in wtechs.items():
+            if wtech != 'nan':
+                output.write(
+                    utech.lstrip() +
+                    ',' +
+                    wtech.lstrip() +
+                    ',' +
+                    str(count) + '\n'
+                )
 
 
 if __name__ == "__main__":
