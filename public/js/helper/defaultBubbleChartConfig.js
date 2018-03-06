134a49e48c9d18f85c195ecbc5b514a5081fa58f
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-06 14:34:20 +0100

    update tooltip to only show bubble label

--- public/js/helper/defaultBubbleChartConfig.js
+++ public/js/helper/defaultBubbleChartConfig.js
@@ -2,6 +2,16 @@ export var defaultconfig = {
     type: "bubble",
     responsive: true,
     options: {
+        tooltips: {
+            callbacks: {
+                title: (tooltipItem, data) => {
+                    return data.datasets[tooltipItem[0].datasetIndex].label
+                },
+                label: () => {
+                    return null
+                },
+            },
+        },
         responsive: true,
         legend: {
             display: false
