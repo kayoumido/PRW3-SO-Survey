c7854605164616ef8079d731dd961f16960792f8
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-07 11:03:58 +0100

    convert wanted thechs into array of objects
    Since the conversion of the original data into an array of objects, the wantedtechs needed to also be converted otherwise the chart generator wouldn't be able to generate a chid chart

--- public/js/helper/createChart.js
+++ public/js/helper/createChart.js
@@ -3,17 +3,35 @@ import { generateCoordinates } from "./generateCoordinates.js";
 
 export function createChart(data, config, divider) {
     let chartData = [];
+    
+    if (!Array.isArray(data)) {
+        // convert object into array of objects
+        let d = [];
+        for (var tech in data) {
+            data[tech] = {
+                key: tech,
+                count: data[tech] 
+            };
+            d.push(data[tech]);
+        }
+        // sort new array
+        d.sort((a, b) => {
+            return a.count - b.count;
+        });
+        
+        data = d;
+    }
+    
     // generate data array
-    for (let tech in data) {
-        let radius = data[tech].count || data[tech];
-
+    for (let tech of data) {
+        let radius = tech.count || tech;
         chartData.push({
-            label: tech,
+            label: tech.key,
             backgroundColor: generatePastelColors().toString(),
-            data: [generateCoordinates(radius / divider)]
+            data: [generateCoordinates(tech.count, divider)]
         });
     }
-
+    
     return new Chart(
         document.getElementById("bubblechart"),
         Object.assign(config, {
