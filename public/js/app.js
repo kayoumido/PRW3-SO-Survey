29daaa5e3c54eb51c23b606d983df797c520e98e
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-07 11:02:37 +0100

    sort data
    The is sorted so that the smaller bubbles stay on top of the bigger ones

--- public/js/app.js
+++ public/js/app.js
@@ -9,17 +9,34 @@ $.ajax({
     method: "GET",
     dataType: "json",
     cache: "false"
-}).done(data => {
+}).done(d => {
+    // copy config
     let parentChartConfig = defaultconfig;
     
+    // convert object of objects into array of objects
+    let data = [];
+    for (var tech in d) {
+        // add a key with the orignial objects key
+        d[tech].key = tech;
+        data.push(d[tech]);
+    }
+    // sort new array
+    data.sort((a, b) => {
+        return a.count - b.count;
+    });
+
     parentChartConfig.options.onClick = (e, element) => {
         
         // check if a bubble was clicked
         if (element[0]) {
-            let clickedBubble = Object.keys(data)[element[0]._datasetIndex];
-            let wantedTechs = data[clickedBubble]["Wanted Technologies"];
-            chart.destroy()
-            childChart = createChart(wantedTechs, defaultconfig, 10)
+            // check if there is a child chart
+            if (childChart == null || childChart.canvas == null) {
+                // no, create it
+                let clickedBubble = Object.keys(data)[element[0]._datasetIndex];
+                let wantedTechs = data[clickedBubble]["Wanted Technologies"];
+                chart.destroy();
+                childChart = createChart(wantedTechs, defaultconfig, 50);
+            }
         }
         else {
             // check if there is a parent chart
