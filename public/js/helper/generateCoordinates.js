cc07b85dde6f4cbf6b72a2cd31b3d2b9c9978d4a
Doran Kayoumi <doran.kayoumi@cpnv.ch>
2018-03-07 11:05:10 +0100

    add tech count to bubble coordinates

--- public/js/helper/generateCoordinates.js
+++ public/js/helper/generateCoordinates.js
@@ -1,28 +1,11 @@
-export function generateCoordinates(radius) {
+export function generateCoordinates(radius, divider) {
+
     var newcircle = {
         x: Math.floor(Math.random() * 100 + 5),
         y: Math.floor(Math.random() * 100 + 5),
-        r: radius
+        r: radius/divider < 5 ? 5 : radius/divider,
+        c: radius
     };
-
-    // if (oldCirclesData.length > 0) {
-    //     for (let oldcircle of oldCirclesData) {
-    //         let foo = false;
-    //
-    //         while (!foo) {
-    //             newcircle.x = Math.floor(Math.random() * 100 + 5);
-    //             newcircle.y = Math.floor(Math.random() * 100 + 5);
-    //
-    //             let dx = oldcircle.x - newcircle.x;
-    //             let dy = oldcircle.y - newcircle.y;
-    //             let dist = Math.sqrt(dx * dx + dy * dy);
-    //
-    //             console.log(dist < newcircle.r + oldcircle.r);
-    //
-    //             if (dist > newcircle.r + oldcircle.r) foo = true;
-    //         }
-    //     }
-    // }
 
     // oldCirclesData.push(newcircle);
     return newcircle;
